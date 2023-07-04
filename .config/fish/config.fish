1aed08162405b7f98328fbbb69ff3fd7b1db8aa7
Luke Davidson <luke@lukedavidson.org>
2023-07-04 02:48:25 -0700

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    abbr --add k kubectl
+    alias config '/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+end
