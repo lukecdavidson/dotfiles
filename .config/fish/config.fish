f36f9c24827a7f7955b937540227bc42476440b4
Luke Davidson <luke@lukedavidson.org>
2023-07-04 02:54:26 -0700

    Added user local bin to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     abbr --add k kubectl
     alias config '/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+    fish_add_path ~/.local/bin
 end
